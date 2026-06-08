54e52d92bd397615455b05f60e9e18d1f57de098
arun <arun@gmail.com>
2026-06-08 20:28:54 +0530

    fix: simplify CORS policy to allow any origin

--- backend/src/infrastructure/app.ts
+++ backend/src/infrastructure/app.ts
@@ -5,20 +5,7 @@ import { HttpStatus } from '../config/httpStatus';
 export const createApp = (): express.Application => {
   const app = express();
 
-  const allowedOrigins = process.env.ALLOWED_ORIGINS
-    ? process.env.ALLOWED_ORIGINS.split(',')
-    : ['http://localhost:5173'];
-
-  app.use(cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true,
-  }));
+  app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
